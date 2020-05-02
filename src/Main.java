a930af57db03e02a0681b5b334e6d7e337ace84a
Mikhail Abramov <unsortedhashsets@gmail.com>
2020-05-02 17:16:16 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import gui.Test;
+
+/**
+ * Main program class - Provides window and scene creation
+ * 
+ * @author Mikhail Abramov (xabram00)
+ * @author Serhii Salatskyi (xsalat00)
+ *
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+        // Parent root = FXMLLoader.load(getClass().getResource("gui/scene.fxml")); some
+        // example
+        Test.HelloWorld();
+        
+        Group root = new Group();
+
+        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+        // scene.getStylesheets().add("gui/mainScene.css");
+
+        Rectangle r = new Rectangle(25, 25, 250, 250);
+        r.setFill(Color.BLUE);
+
+        root.getChildren().add(r);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("IJA-JA-JA-JA");
+
+        primaryStage.show();
+
+    }
+
+    public int getInt() {
+        return 1;
+    }
+}
---
+++ src/gui/Test.java
@@ -0,0 +1,15 @@
+package gui;
+
+/**
+ * TEST class - does nothing just for fun
+ * 
+ * @author Mikhail Abramov (xabram00)
+ * @author Serhii Salatskyi (xsalat00)
+ * 
+ * 
+ */
+public class Test {
+    public static void HelloWorld() {
+        System.out.println("Hello World");
+    }
+}
