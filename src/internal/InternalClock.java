0f7b00aff9181d185154d414cd8e58e874687234
Salatskyi Serhii <ssalatsk@redhat.com>
2020-05-07 23:06:50 +0200

    Return clock's value as string

--- src/internal/InternalClock.java
+++ src/internal/InternalClock.java
@@ -4,6 +4,7 @@ import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public class InternalClock {
     private float acceleration;
@@ -18,10 +19,12 @@ public class InternalClock {
         this.clock = Clock.fixed(Instant.parse("2020-05-01T10:00:00.00Z"), ZoneId.of("Europe/Prague"));
     }
 
-    public Clock updateClock(){
-        long millis = (long) acceleration * 100;
+    public String updateClock(){
+        long millis = (long) acceleration * 40;
         this.clock = Clock.offset(this.clock, Duration.ofMillis(millis));
-        return this.clock;
+
+        String localTime = this.clock.instant().atZone(ZoneOffset.UTC).toLocalTime().toString();
+        return localTime.substring(0, 8); // get time without millis
     }
 
     public void increaseAccelerationLevel(){
